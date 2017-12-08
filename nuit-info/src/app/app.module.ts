f169457041b335bdafe7e1f039527f444cd570fa
EpicKiwi <me@epickiwi.fr>
2017-12-08 04:33:58 +0100

    Interface

--- nuit-info/src/app/app.module.ts
+++ nuit-info/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { MessageComponent } from './message/message.component';
+import { OptionComponent } from './option/option.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageComponent,
+    OptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
