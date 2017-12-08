fcd7fa3b5f0c81d579c09ff4d4924ad817c920f6
Jean-GuillaumePonsard <jeanguillaume.ponsard@viacesi.fr>
2017-12-08 01:34:44 +0100

    Structure basique du projet

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var app = require('express')(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server),
+	ent = require('ent'), //equalivalent à htmlentities en php
+	fs = require('fs');
 
-var app = express();
+app.get('/', function(require, res){
+	res.sendFile(__dirname + '/view/index.html');
+});
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+io.sockets.on('connection', function(socket, token){
+	socket.on('nouveau_client', function(token){
+		if(token === null)
+		{
+			//generate token to save the user
+		}
+		socket.token = token;
+		//Create the User object
+	});
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+	socket.on('message', function(message){
+		// Décrypter le message pour l'analyser
 
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/view/index.html")
-})
+	});
+
+});
 
 app.listen(8080,()=>{
   console.log("Listening on *:8080")
